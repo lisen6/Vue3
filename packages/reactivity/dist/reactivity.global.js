2ab25a1d03722b67feb7b0e589f432878f6532f2
sen.li <sen.li@hexagon.com>
2022-06-12 22:48:59 +0800

    add a vue3 example

---
+++ packages/reactivity/dist/reactivity.global.js
@@ -0,0 +1,10 @@
+var VueReactivity = (() => {
+  // packages/shared/src/index.ts
+  var isObject = (value) => {
+    return typeof value === "object" && value !== null;
+  };
+
+  // packages/reactivity/src/index.ts
+  console.log(isObject({}));
+})();
+//# sourceMappingURL=reactivity.global.js.map
